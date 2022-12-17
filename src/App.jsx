d9fb60713bfd15af3371d783fb6e266e741e6309
JINAJina <wlsdk120@gmail.com>
2022-12-17 19:26:32 +0900

    feat: router 추가

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,15 @@
+import Router from './shared/Router';
 function App() {
+<<<<<<< Updated upstream
   return <div>Home</div>;
+=======
+  return (
+    <div>
+      Home 안녕
+      <Router />
+    </div>
+  );
+>>>>>>> Stashed changes
 }
 
 export default App;
---
+++ src/pages/PostUpload.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Postupload = () => {
+  return <div>Postupload</div>;
+};
+
+export default Postupload;

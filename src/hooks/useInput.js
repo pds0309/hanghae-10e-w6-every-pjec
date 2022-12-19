4568a0b84bcd6344147277b0c7e50c3bcfa213fe
heayounchoi <heayounchoi@gmail.com>
2022-12-19 10:12:22 +0900

    style: 변수명, styled-component 위치 수정

--- src/hooks/useInput.js
+++ src/hooks/useInput.js
@@ -1,10 +1,10 @@
 import { useState } from 'react';
 
-const useInput = (initValue, checkType, pwCheck) => {
+const useInput = (initValue, checkType, compareCheck) => {
   const [value, setValue] = useState(initValue);
 
-  let validation = false;
-  let validationText = '';
+  let isValid = false;
+  let validText = '';
 
   const IDCheck = /^(?=.*[A-Z])(?=.*[a-z])(?=.*[0-9])[A-Za-z0-9]{3,}$/;
 
@@ -12,50 +12,53 @@ const useInput = (initValue, checkType, pwCheck) => {
     switch (checkType) {
       case 'loginId':
         IDCheck.test(value) && value.length >= 4 && value.length <= 12
-          ? (validation = true)
-          : (validation = false);
+          ? (isValid = true)
+          : (isValid = false);
 
-        validation
-          ? (validationText = '사용 가능한 ID입니다.')
-          : (validationText =
+        isValid
+          ? (validText = '사용 가능한 ID입니다.')
+          : (validText =
               'ID는 영문 대문자, 소문자, 숫자를 포함하며, 4자 이상 12자 이하여야합니다.');
-
         break;
+
       case 'nickname':
-        value.length >= 4 && value.length <= 12 ? (validation = true) : (validation = false);
+        value.length >= 4 && value.length <= 12 ? (isValid = true) : (isValid = false);
 
-        validation
-          ? (validationText = '사용 가능한 닉네임입니다.')
-          : (validationText = '닉네임은 4자 이상 12자 이하여야합니다.');
+        isValid
+          ? (validText = '사용 가능한 닉네임입니다.')
+          : (validText = '닉네임은 4자 이상 12자 이하여야합니다.');
         break;
+
       case 'password':
-        if (pwCheck.length !== 0) {
-          value.length >= 4 && value.length <= 12 && !value.includes(pwCheck)
-            ? (validation = true)
-            : (validation = false);
+        if (compareCheck.length !== 0) {
+          value.length >= 4 && value.length <= 12 && !value.includes(compareCheck)
+            ? (isValid = true)
+            : (isValid = false);
         } else {
-          value.length >= 4 && value.length <= 12 ? (validation = true) : (validation = false);
+          value.length >= 4 && value.length <= 12 ? (isValid = true) : (isValid = false);
         }
 
-        validation
-          ? (validationText = '사용 가능한 비밀번호입니다.')
-          : (validationText =
-              '비밀번호는 4자 이상 12자 이하여야하며, 아이디를 포함하지 않아야합니다.');
+        isValid
+          ? (validText = '사용 가능한 비밀번호입니다.')
+          : (validText = '비밀번호는 4자 이상 12자 이하여야하며, 아이디를 포함하지 않아야합니다.');
         break;
+
       case 'pwconfirm':
-        value.length !== 0 && value === pwCheck ? (validation = true) : (validation = false);
-        validation
-          ? (validationText = '비밀번호가 일치합니다.')
-          : (validationText = '비밀번호가 일치하지 않습니다.');
+        value.length !== 0 && value === compareCheck ? (isValid = true) : (isValid = false);
+
+        isValid
+          ? (validText = '비밀번호가 일치합니다.')
+          : (validText = '비밀번호가 일치하지 않습니다.');
         break;
+
       default:
-        validationText = 'error: Input Error';
+        validText = 'error: Input Error';
     }
   }
 
   const onChange = e => setValue(e.target.value);
 
-  return [value, onChange, validation, validationText];
+  return [value, onChange, isValid, validText];
 };
 
 export default useInput;
--- src/pages/Signup.jsx
+++ src/pages/Signup.jsx
@@ -1,18 +1,120 @@
 import React from 'react';
+import userApi from '.././apis/userApi';
+
+import { useNavigate } from 'react-router-dom';
+import useInput from '../hooks/useInput';
+
+import styled from 'styled-components';
 import Button from '../components/common/Button';
 import Input from '../components/common/Input';
 import Label from '../components/common/Label';
 import ValidationText from '../components/common/ValidationText';
-import useInput from '../hooks/useInput';
-import { useNavigate } from 'react-router-dom';
-import styled from 'styled-components';
-import userApi from '.././apis/userApi';
+
+const Signup = () => {
+  const navigate = useNavigate();
+  const [loginId, onChangeID, isValidID, validTextID] = useInput('', 'loginId');
+  const [nickname, onChangeNickname, isValidNickname, validTextNickname] = useInput('', 'nickname');
+
+  const idCheck = loginId;
+  const [password, onChangePW, isValidPW, validTextPW] = useInput('', 'password', idCheck);
+
+  const pwCheck = password;
+  const [pwconfirm, onChangePWconfirm, isValidPWconfirm, validTextPWConfirm] = useInput(
+    '',
+    'pwconfirm',
+    pwCheck,
+  );
+  //중복검사는 onBlur
+  const postUserInfo = async e => {
+    e.preventDefault();
+    if (isValidID && isValidNickname && isValidPW && isValidPWconfirm) {
+      try {
+        await userApi.join({
+          loginId,
+          nickname,
+          password,
+          pwconfirm,
+        });
+      } catch (e) {
+        alert(e);
+      }
+    }
+    navigate('/signin');
+  };
+
+  return (
+    <Wrap>
+      <Title>
+        <span style={{ color: '#CE7777' }}>SERVICE</span> NAME 회원가입
+      </Title>
+      <FormWrap onSubmit={postUserInfo}>
+        <Label>로그인 ID</Label>
+        <Input
+          width="366px"
+          height="30px"
+          placeholder="ID를 입력하세요."
+          value={loginId}
+          onChange={onChangeID}
+          autoFocus
+          required
+        />
+        <ValidationText isValidationSuccess={isValidID}>{validTextID}</ValidationText>
+        <Label>닉네임</Label>
+        <Input
+          width="366px"
+          height="30px"
+          placeholder="닉네임을 입력하세요."
+          value={nickname}
+          onChange={onChangeNickname}
+          required
+        />
+        <ValidationText isValidationSuccess={isValidNickname}>{validTextNickname}</ValidationText>
+        <Label>비밀번호</Label>
+        <Input
+          width="366px"
+          height="30px"
+          placeholder="비밀번호를 입력하세요."
+          value={password}
+          onChange={onChangePW}
+          required
+        />
+        <ValidationText isValidationSuccess={isValidPW}>{validTextPW}</ValidationText>
+        <Label>비밀번호 확인</Label>
+        <Input
+          width="366px"
+          height="30px"
+          placeholder="비밀번호를 입력하세요."
+          value={pwconfirm}
+          onChange={onChangePWconfirm}
+          required
+        />
+        <ValidationText isValidationSuccess={isValidPWconfirm}>{validTextPWConfirm}</ValidationText>
+        <ButtonWrap>
+          <Button style={{ height: '50px' }}>회원가입</Button>
+          <Button
+            type="button"
+            style={{ height: '50px' }}
+            onClick={() => navigate(-1)}
+            btnTheme={'secondary'}
+          >
+            뒤로가기
+          </Button>
+        </ButtonWrap>
+      </FormWrap>
+    </Wrap>
+  );
+};
 
 const Wrap = styled.div`
   display: flex;
   flex-direction: column;
   justify-content: center;
   align-items: center;
+`;
+
+const Title = styled.span`
+  font-size: 32px;
+  font-weight: 600;
 `;
 
 const FormWrap = styled.form`
@@ -36,96 +138,5 @@ const ButtonWrap = styled.div`
   flex-direction: column;
   justify-content: space-between;
 `;
-
-const Signup = () => {
-  const navigate = useNavigate();
-  const [loginId, onChangeID, validID, validTextID] = useInput('', 'loginId');
-  const [nickname, onChangeNickname, validNickname, validTextNickname] = useInput('', 'nickname');
-  const pwIncludeIDCheck = loginId;
-  const [password, onChangePW, validPW, validTextPW] = useInput('', 'password', pwIncludeIDCheck);
-  const pwCheck = password;
-  const [pwconfirm, onChangePWconfirm, validPWconfirm, validTextPWConfirm] = useInput(
-    '',
-    'pwconfirm',
-    pwCheck,
-  );
-  //중복검사는 onBlur
-  const postUserInfo = async e => {
-    e.preventDefault();
-    if (validID && validNickname && validPW && validPWconfirm) {
-      try {
-        await userApi.join({
-          loginId,
-          nickname,
-          password,
-          pwconfirm,
-        });
-      } catch (e) {
-        alert(e);
-      }
-    }
-    navigate('/signin');
-  };
-
-  return (
-    <Wrap>
-      <Label>회원가입</Label>
-      <FormWrap onSubmit={postUserInfo}>
-        <Label>로그인 ID</Label>
-        <Input
-          width="366px"
-          height="30px"
-          placeholder="ID를 입력하세요."
-          value={loginId}
-          onChange={onChangeID}
-          autoFocus
-          required
-        />
-        <ValidationText isValidationSuccess={validID}>{validTextID}</ValidationText>
-        <Label>닉네임</Label>
-        <Input
-          width="366px"
-          height="30px"
-          placeholder="닉네임을 입력하세요."
-          value={nickname}
-          onChange={onChangeNickname}
-          required
-        />
-        <ValidationText isValidationSuccess={validNickname}>{validTextNickname}</ValidationText>
-        <Label>비밀번호</Label>
-        <Input
-          width="366px"
-          height="30px"
-          placeholder="비밀번호를 입력하세요."
-          value={password}
-          onChange={onChangePW}
-          required
-        />
-        <ValidationText isValidationSuccess={validPW}>{validTextPW}</ValidationText>
-        <Label>비밀번호 확인</Label>
-        <Input
-          width="366px"
-          height="30px"
-          placeholder="비밀번호를 입력하세요."
-          value={pwconfirm}
-          onChange={onChangePWconfirm}
-          required
-        />
-        <ValidationText isValidationSuccess={validPWconfirm}>{validTextPWConfirm}</ValidationText>
-        <ButtonWrap>
-          <Button style={{ height: '50px' }}>회원가입</Button>
-          <Button
-            type="button"
-            style={{ height: '50px' }}
-            onClick={() => navigate(-1)}
-            btnTheme={'secondary'}
-          >
-            뒤로가기
-          </Button>
-        </ButtonWrap>
-      </FormWrap>
-    </Wrap>
-  );
-};
 
 export default Signup;

c22a8615fd7b6d382736c7f0ef48d18046154d61
pds0309 <76927397+pds0309@users.noreply.github.com>
2022-12-19 18:18:46 +0900

    feat: 인증 상태에 따른 페이지 접근 설정 추가

--- src/shared/Router.jsx
+++ src/shared/Router.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { BrowserRouter, Navigate, Route, Routes } from 'react-router-dom';
 import Home from '../pages/Home';
 import Post from '../pages/Post';
 import PostDetail from '../pages/PostDetail';
@@ -19,12 +19,12 @@ const Router = () => {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="post" element={<Post />} />
-        <Route path="postdetail" element={<PostDetail />} />
-        <Route path="postupload" element={<PostUpload />} />
-        <Route path="profile" element={<Profile />} />
-        <Route path="signin" element={<Signin />} />
-        <Route path="signup" element={<Signup />} />
-        <Route path="bookmark" element={<BookMark />} />
+        <Route path="postdetail:postId" element={<PostDetail />} />
+        <Route path="postupload" element={isLogined ? <PostUpload /> : <Navigate to="/" />} />
+        <Route path="profile" element={isLogined ? <Profile /> : <Navigate to="/" />} />
+        <Route path="signin" element={!isLogined ? <Signin /> : <Navigate to="/" />} />
+        <Route path="signup" element={!isLogined ? <Signup /> : <Navigate to="/" />} />
+        <Route path="bookmark" element={isLogined ? <BookMark /> : <Navigate to="/" />} />
       </Routes>
     </BrowserRouter>
   );

360478c4dbbdaf83fc82931e0fd8925cc48f58a2
heayounchoi <heayounchoi@gmail.com>
2022-12-22 23:21:36 +0900

    practice: 내 작성글 조회 기능 연습

--- src/pages/Post.jsx
+++ src/pages/Post.jsx
@@ -1,34 +1,42 @@
 import React, { useEffect, useState } from 'react';
-import { useSelector } from 'react-redux';
+import { useSelector, useDispatch } from 'react-redux';
 import styled from 'styled-components';
 import LinkGroup from '../components/common/LinkGroup';
 import PostCard from '../components/post/PostCard';
 import { __myPostById } from '../redux/modules/PostSlice';
-import { useDispatch } from 'react-redux';
 
 const Post = () => {
   const dispatch = useDispatch();
-  const { posts } = useSelector(state => state.posts);
+  // 7. store 파일에 리듀서가 넘겨준 상태값을 가져오기
+  const myPosts = useSelector(state => state.posts.post);
+  // 8. 컴포넌트 안에서 map 돌렸다가 filter 돌렸다가 할 수 있게 state로 값 관리하기
+  // 처음 화면엔 전체 데이터를 표시해주니 초기값으로 받아온 값을 설정해주기
+  const [postList, setPostList] = useState(myPosts);
 
-  const [postList, setPostList] = useState([]);
-
-  useEffect(() => {
-    fetchTodos();
-    // if (posts) {
-    //   setPostList(posts);
-    // }
-  }, []);
+  // 밑에서 실행시킬 함수
   const fetchTodos = () => {
     dispatch(__myPostById());
   };
 
+  // 첫 컴포넌트 렌더링 시 리덕스를 실행시킴 (리덕스 파일로 고고우)
+  useEffect(() => {
+    fetchTodos();
+  }, []);
+
   // 임시필터링 함수
   const getPostFilteredBy = param => {
-    console.log(postList);
-    const filterpost = postList.mePost.filter(post => post.division === param);
-    console.log(filterpost);
-    setPostList(postList?.filter(post => post.division === param));
+    // 10. 필터 함수로 사용될 myPosts나 postList 값이 잘 들어오는지 콘솔로 확인하기
+    // 11. 필터할때 필터의 필터의 필터의 필터가 되지 않게 전체값으로 필터하기
+    // 생각해보니까 상수 따로 안만드시고 그냥 setPostList에 함수 넣으셔도 될 것 같아용
+    // setPostList(myPosts?.filter(post => post.division === param)); <== 이런식으로!!
+    // 중요한건 '전체 데이터'를 필터링하는 것입니다
+    const filterpost = myPosts?.filter(post => post.division === param);
+    // 12. 필터가 잘 됐는지 콘솔 찍어보기
+    setPostList(filterpost);
+    // 13. state값이 잘 설정됐는지 콘솔 찍어보기
+    // 끝 ~~~~~~
   };
+
   return (
     <Wrap>
       <Title>내 작성글</Title>
@@ -40,9 +48,9 @@ const Post = () => {
           () => getPostFilteredBy('스터디'),
         ]}
       />
-
+      {/* 9. 맵으로 전체 카드를 보여주는지 확인하기 */}
       <CardListContainer>
-        {postList.mePost?.map(post => (
+        {postList?.map(post => (
           <PostCard key={post.postId} post={post} />
         ))}
       </CardListContainer>
--- src/redux/modules/PostSlice.js
+++ src/redux/modules/PostSlice.js
@@ -1,6 +1,7 @@
 import { createAsyncThunk, createSlice } from '@reduxjs/toolkit';
 import postApi from '../../apis/postApi';
 
+// 1. 초기 데이터 형태
 const initialState = {
   posts: [],
   post: null,
@@ -36,11 +37,15 @@ export const __deletePostById = createAsyncThunk('deletePost', async (payload, t
     return thunkAPI.rejectWithValue(error);
   }
 });
-export const __myPostById = createAsyncThunk('myPost', async (payload, thunkAPI) => {
+
+export const __myPostById = createAsyncThunk('myPost', async (_, thunkAPI) => {
+  // 2. 함수로 들어왔는지 콘솔 찍어보기
   try {
+    // 3. try문으로 들어오는지 또 콘솔 찍어보기
     const { data } = await postApi.getMyPosts();
-    console.log(data);
-    return thunkAPI.fulfillWithValue(data);
+    // 4. 데이터 잘 받아왔는지 콘솔 찍어보기
+    // 데이터 타입이 object이면 . . . 쓰면서 쭉쭉 들어가면 됨
+    return thunkAPI.fulfillWithValue(data.mePost);
   } catch (error) {
     return thunkAPI.rejectWithValue(error);
   }
@@ -93,13 +98,14 @@ export const postSlice = createSlice({
       state.isLoading = false;
       state.error = action.payload;
       state.deleteSuccess = false;
-    },
+    }, // 5. 데이터를 받아오는 중, 받음, 에러 떴을때를 나눠서 상태 관리해주기
     [__myPostById.pending]: state => {
       state.isLoading = true;
     },
     [__myPostById.fulfilled]: (state, action) => {
       state.isLoading = false;
       state.post = action.payload;
+      // 6. 데이터가 상태값으로 잘 들어갔는지 콘솔로 확인하기
     },
     [__myPostById.rejected]: (state, action) => {
       state.isLoading = false;

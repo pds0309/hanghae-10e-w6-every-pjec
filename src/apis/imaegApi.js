512643d2a74fb48ba7847347aa4a7a77fd126124
pds0309 <76927397+pds0309@users.noreply.github.com>
2022-12-22 17:31:12 +0900

    feat: 텍스트 에디터 추가

--- src/apis/imaegApi.js
+++ src/apis/imaegApi.js
@@ -5,6 +5,7 @@ const IMAGE_API = '/api/v1/images';
 const imageApi = {
   // 프로필 등록
   uploadProfile: formData => imageInstance.post(IMAGE_API + '/profile', formData),
+  uploadPost: formData => imageInstance.post(IMAGE_API + '/posts', formData),
 };
 
 export default imageApi;
